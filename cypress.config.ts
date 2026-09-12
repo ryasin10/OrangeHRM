43a8f079cd9f612b526d9f32cb45926cd7723e35
ReemYasin <reem.yasin789@gmail.com>
2026-09-12 19:22:38 +0300

    Update Cypress configuration and dependencies

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,35 @@
 import { defineConfig } from "cypress";
+import webpackPreprocessor from "@cypress/webpack-preprocessor";
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
 
 export default defineConfig({
   allowCypressEnv: false,
-
   e2e: {
+    baseUrl: "https://opensource-demo.orangehrmlive.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on(
+        "file:preprocessor",
+        webpackPreprocessor({
+          webpackOptions: {
+            resolve: {
+              extensions: [".ts", ".js"],
+              plugins: [
+                new TsconfigPathsPlugin({ configFile: "./tsconfig.json" }),
+              ],
+            },
+            module: {
+              rules: [
+                {
+                  test: /\.ts$/,
+                  exclude: [/node_modules/],
+                  use: [{ loader: "ts-loader" }],
+                },
+              ],
+            },
+          },
+        }),
+      );
+      return config;
     },
   },
 });
